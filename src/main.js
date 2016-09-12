df16a33449e11f9ed61171e0e48ead9cee211ed6
JeOam <lxjamsterdam@gmail.com>
2016-09-12 23:14:11 +0800

    update webpack config

--- src/main.js
+++ src/main.js
@@ -1,35 +1,41 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import VueResource from 'vue-resource'
-import VueAwesome from 'vue-awesome'
-import NProgress from 'nprogress'
+// import VueRouter from 'vue-router'
+// import VueResource from 'vue-resource'
+// import VueAwesome from 'vue-awesome'
+// import NProgress from 'nprogress'
 import App from './App'
-import configRouter from './routes'
+// import configRouter from './routes'
+//
+// Vue.use(VueRouter)
+// Vue.use(VueResource)
+//
+// Vue.http.options.root = 'http://127.0.0.1:8000'
+// Vue.http.interceptors.push(() => {
+//   return {
+//     request (request) {
+//       NProgress.start()
+//       return request
+//     },
+//     response (response) {
+//       NProgress.done()
+//       return response
+//     }
+//   }
+// })
+//
+// Vue.component('icon', VueAwesome)
+//
+// const router = new VueRouter({
+//   history: true,
+//   saveScrollPosition: true,
+//   suppressTransitionError: true
+// })
+//
+// configRouter(router)
+// router.start(App, '#app')
 
-Vue.use(VueRouter)
-Vue.use(VueResource)
-
-Vue.http.options.root = 'http://127.0.0.1:8000'
-Vue.http.interceptors.push(() => {
-  return {
-    request (request) {
-      NProgress.start()
-      return request
-    },
-    response (response) {
-      NProgress.done()
-      return response
-    }
-  }
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
 })
-
-Vue.component('icon', VueAwesome)
-
-const router = new VueRouter({
-  history: true,
-  saveScrollPosition: true,
-  suppressTransitionError: true
-})
-
-configRouter(router)
-router.start(App, '#app')
