5a534b7cbcad87580bccd8d20894c610779a08ed
JeOam <lxjamsterdam@gmail.com>
2016-09-14 11:06:12 +0800

    improve navigation bar

--- src/components/layout/Navigation.vue
+++ src/components/layout/Navigation.vue
@@ -1,46 +1,54 @@
 <template>
-  <nav class="nav nav-container">
-    <div class="nav-left">
-      <a class="nav-item is-brand" href="#">
-        <img src="../../assets/bulma.png" alt="Bulma logo">
-      </a>
-      <p class="nav-item control">
-        <input class="input" type="text" placeholder="Search a post">
-        <button class="button">
-          Search
-        </button>
-      </p>
-    </div>
+  <nav class="nav has-shadow nav-container">
+    <div class="container">
+      <div class="nav-left">
+        <a class="nav-item is-tab is-active"><i class="fa fa-home"></i> &nbsp; Home</a>
+        <a class="nav-item is-tab"><i class="fa fa-bolt"></i> &nbsp; API tools</a>
+      </div>
 
-    <span @click="toggleNav" class="nav-toggle">
-      <span></span>
-      <span></span>
-      <span></span>
-    </span>
-
-    <div id="nav-menu" class="nav-right nav-menu">
-      <a class="nav-item" href="#">Home</a>
-      <a class="nav-item" href="#">Documentation</a>
-      <a class="nav-item" href="#">Blog</a>
-      <span class="nav-item">
-        <a class="button is-primary">
+      <div class="nav-center">
+        <a class="nav-item" href="#">
           <span class="icon">
-            <i class="fa fa-github"></i>
+            <i class="fa fa-twitter"></i>
           </span>
-          <span>GitHub</span>
         </a>
+      </div>
+
+      <span @click="toggleNav" class="nav-toggle" :class="{'is-active': navMenuOpen}">
+        <span></span>
+        <span></span>
+        <span></span>
       </span>
+
+      <div id="nav-menu" class="nav-right nav-menu" :class="{'is-active': navMenuOpen}">
+        <span class="nav-item">
+          <input class="input" type="text" placeholder="Search a post">
+          <button class="button">
+            Search
+          </button>
+        </span>
+        <span class="nav-item">
+          <a class="button is-primary">
+            <span class="icon">
+              <i class="fa fa-github"></i>
+            </span>
+            <span>GitHub</span>
+          </a>
+        </span>
+      </div>
     </div>
   </nav>
 </template>
 <script>
-import $ from 'jquery'
-
 export default {
+  data () {
+    return {
+      navMenuOpen: false
+    }
+  },
   methods: {
     toggleNav (e) {
-      $(e.target).toggleClass('is-active')
-      $('#nav-menu').toggleClass('is-active')
+      this.navMenuOpen = !this.navMenuOpen
     }
   }
 }
@@ -49,6 +57,5 @@ export default {
 <style scoped>
 .nav-container {
   background-color: #f5f5f5;
-  border-bottom: 1px solid #e5e5e5;
 }
 </style>
